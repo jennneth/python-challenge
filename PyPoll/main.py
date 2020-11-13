4a61b4680eb0a2f661ddda88f83018a106ce447c
jennneth <johansson_jen@hotmail.com>
2020-11-13 13:56:07 -0800

    Added problem statement to file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,4 +1,37 @@
 #copied code from PyBank, will modify
+#PyPoll program
+#*************   Assignment:    ***********************
+#  In this challenge, you are tasked with helping a small, rural town modernize its vote counting process.
+#* You will be give a set of poll data called [election_data.csv](PyPoll/Resources/election_data.csv). The dataset is composed of three columns: `Voter ID`, `County`, and `Candidate`. Your task is to create a Python script that analyzes the votes and calculates each of the following:
+
+#  * The total number of votes cast
+
+# * A complete list of candidates who received votes
+
+#  * The percentage of votes each candidate won
+
+#  * The total number of votes each candidate won
+
+#  * The winner of the election based on popular vote.
+
+#* As an example, your analysis should look similar to the one below:
+
+#  ```text
+#  Election Results
+#  -------------------------
+#  Total Votes: 3521001
+#  -------------------------
+#  Khan: 63.000% (2218231)
+#  Correy: 20.000% (704200)
+#  Li: 14.000% (492940)
+#  O'Tooley: 3.000% (105630)
+#  -------------------------
+#  Winner: Khan
+#  -------------------------
+#  ```
+
+#* In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+#************ PROGRAM STARTS***********************
 ## import modules
 import os
 import csv
@@ -17,10 +50,10 @@ date_largest = 0 #initialize the variable to hold the month of the largest delta
 date_smallest = 0 #initialize the variable to hold the month of the smallest delta
 
 ## read csv file
-finance_csv = os.path.join('.', 'Resources', 'budget_data.csv')
+vote_csv = os.path.join('.', 'Resources', 'election_data.csv')
 
 ##open csv file
-with open(finance_csv, encoding="utf-8") as csvfile:
+with open(vote_csv, encoding="utf-8") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     #since there is a header row, skip reading the first row into the lists
     csv_header = next(csvreader)
